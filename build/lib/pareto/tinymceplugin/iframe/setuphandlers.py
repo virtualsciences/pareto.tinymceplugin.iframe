670b42ffd39352bf12d0856edd15807b53856d0a
Thijs Jonkman <t.jonkman@gmail.com>
2012-10-24 16:14:24 +0200

    Added setup handlers

---
+++ build/lib/pareto/tinymceplugin/iframe/setuphandlers.py
@@ -0,0 +1,19 @@
+from plone.app.controlpanel.filter import FilterControlPanelAdapter
+
+def setupVarious(context):
+
+    # Ordinarily, GenericSetup handlers check for the existence of XML files.
+    # Here, we are not parsing an XML file, but we use this text file as a
+    # flag to check that we actually meant for this import step to be run.
+    # The file is found in profiles/default.
+    if context.readDataFile('pareto.tinymceplugin.iframe_various.txt') is None:
+        return
+    
+    # Add additional setup code here
+    portal = context.getSite()
+    FCPA = FilterControlPanelAdapter(portal)
+    if u'iframe' not in FCPA.custom_tags:
+        FCPA.custom_tags.append(u'iframe')
+        FCPA.custom_tags.sort()
+    
+    return
